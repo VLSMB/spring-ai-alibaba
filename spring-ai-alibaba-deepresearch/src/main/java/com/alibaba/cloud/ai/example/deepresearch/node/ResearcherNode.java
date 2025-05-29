dd351a503c3746c959043ffe81cee0457499ca3e
VLSMB <2047857654@qq.com>
2025-05-29 23:08:54 +0800

    feat: fix CoderNode and ResearcherNode

--- spring-ai-alibaba-deepresearch/src/main/java/com/alibaba/cloud/ai/example/deepresearch/node/ResearcherNode.java
+++ spring-ai-alibaba-deepresearch/src/main/java/com/alibaba/cloud/ai/example/deepresearch/node/ResearcherNode.java
@@ -17,6 +17,7 @@
 package com.alibaba.cloud.ai.example.deepresearch.node;
 
 import com.alibaba.cloud.ai.example.deepresearch.model.Plan;
+import com.alibaba.cloud.ai.example.deepresearch.util.TemplateUtil;
 import com.alibaba.cloud.ai.graph.OverAllState;
 import com.alibaba.cloud.ai.graph.action.NodeAction;
 import org.apache.commons.compress.utils.Lists;
@@ -27,9 +28,7 @@ import org.springframework.ai.chat.messages.AssistantMessage;
 import org.springframework.ai.chat.messages.Message;
 import org.springframework.ai.chat.messages.SystemMessage;
 import org.springframework.ai.chat.messages.UserMessage;
-import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.ai.model.tool.ToolCallingChatOptions;
-import org.springframework.ai.tool.ToolCallback;
 
 import java.util.*;
 
@@ -39,16 +38,14 @@ public class ResearcherNode implements NodeAction {
 
 	private final ChatClient researchAgent;
 
-	private final ToolCallback[] toolCallbacks;
-
-	public ResearcherNode(ChatClient researchAgent, ToolCallback[] toolCallbacks) {
+	public ResearcherNode(ChatClient researchAgent) {
 		this.researchAgent = researchAgent;
-		this.toolCallbacks = toolCallbacks;
 	}
 
 	@Override
 	public Map<String, Object> apply(OverAllState state) throws Exception {
-
+		logger.info("Researcher Node is running.");
+		List<Message> messages = TemplateUtil.applyPromptTemplate("researcher", state);
 		Plan currentPlan = state.value("current_plan", Plan.class).get();
 		List<String> observations = state.value("observations", List.class)
 			.map(list -> (List<String>) list)
@@ -56,13 +53,11 @@ public class ResearcherNode implements NodeAction {
 
 		Plan.Step unexecutedStep = null;
 		for (Plan.Step step : currentPlan.getSteps()) {
-			if (step.getExecutionRes() == null) {
+			if (step.getStepType().equals(Plan.StepType.RESEARCH) && step.getExecutionRes() == null) {
 				unexecutedStep = step;
 				break;
 			}
 		}
-
-		List<Message> messages = new ArrayList<>();
 
 		// 添加任务消息
 		Message taskMessage = new UserMessage(
@@ -75,17 +70,21 @@ public class ResearcherNode implements NodeAction {
 				"IMPORTANT: DO NOT include inline citations in the text. Instead, track all sources and include a References section at the end using link reference format. Include an empty line between each citation for better readability. Use this format for each reference:\n- [Source Title](URL)\n\n- [Another Source](URL)");
 		messages.add(citationMessage);
 
+		logger.debug("Researcher Node messages: {}", messages);
 		// 调用agent
 		String content = researchAgent.prompt()
-			.options(ToolCallingChatOptions.builder().toolCallbacks(toolCallbacks).build())
+			.options(ToolCallingChatOptions.builder().build())
+			.toolNames("tavilySearchApi")
 			.messages(messages)
 			.call()
 			.content();
 		unexecutedStep.setExecutionRes(content);
 
+		logger.info("Researcher Node response: {}", content);
 		Map<String, Object> updated = new HashMap<>();
-		updated.put("messages", new AssistantMessage(content));
-		updated.put("observations", observations.add(content));
+		updated.put("messages", List.of(new AssistantMessage(content)));
+		observations.add(content);
+		updated.put("observations", observations);
 
 		return updated;
 	}
